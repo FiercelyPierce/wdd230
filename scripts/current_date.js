652b96a394382ba24cc3bec07b54f68b5f3e7e6d
FiercelyPierce <cir21001@byui.edu>
2022-09-19 20:40:29 -0700

    Added dynamic date

--- scripts/current_date.js
+++ scripts/current_date.js
@@ -1,2 +1,33 @@
 let LastModified = new Date(document.lastModified);
-document.querySelector("#currentdate").textContent = LastModified;
+
+const dayNames = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday"
+];
+
+const monthNames = [
+    "January",
+    "Feburary",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December"
+];
+
+const todaysDate = new Date();
+const dayName = dayNames[todaysDate.getDate()];
+const monthName = monthNames[todaysDate.getDate()];
+const currentDate = `${dayName}, ${todaysDate.getDate()} ${monthName}, ${todaysDate.getFullYear()}`;
+
+document.querySelector("#currentdate").textContent = currentDate;
