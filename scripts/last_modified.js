f25763c178c430e8d026aa92e2fbab95bf75dddc
FiercelyPierce <cir21001@byui.edu>
2022-09-20 18:00:39 -0700

    Finally works...

--- scripts/last_modified.js
+++ scripts/last_modified.js
@@ -1,4 +1,4 @@
 const todaysDate = new Date();
 
-document.getElementsByClassName(".todaysyear").textContent = todaysDate.getFullYear();
+document.querySelector(".todaysyear").textContent = todaysDate.getFullYear();
 document.querySelector("#lastmodified").textContent = document.lastModified;
