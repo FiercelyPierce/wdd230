ac83821487da22d2ab35b52200f62e43f4fbc3d7
FiercelyPierce <cir21001@byui.edu>
2022-09-27 22:50:30 -0700

    Finished

---
+++ lesson2/scripts/last_modified.js
@@ -0,0 +1,4 @@
+const todaysDate = new Date();
+
+document.querySelector(".todaysyear").textContent = todaysDate.getFullYear();
+document.querySelector("#lastmodified").textContent = document.lastModified;
